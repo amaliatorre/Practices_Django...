c994cc8ff3f3aa4b677d873627feadef9b30653e
Amalia Torre <amaliatorrevillahoz@gmail.com>
2024-02-16 10:28:56 +0100

    1 exercice first try

--- Practices/models.py
+++ Practices/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+#models.py
+# Modelo:
+# Crea un modelo llamado Producto con campos como nombre, precio y cantidad en stock.
+# QuerySet:
+# Implementa un QuerySet que filtre los productos con un precio mayor a $50.
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=40)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_quantity = models.PositiveIntegerField()
+
+
+
+
+
+
+
--- Practices/views.py
+++ Practices/views.py
@@ -1,3 +1,15 @@
+from Practices.models import Product
+from typing import List
 from django.shortcuts import render
+#views.py
+def up_to_50_euros(request) -> List[Product]:
 
-# Create your views here.
+    # traer de la bbdd todos los productor que cumplan esta condicion...
+    # crear la lista para mandar
+
+    query_up_to_50_euros = Product.objects.filter(price__gte=50)
+    listo_of_products_up_to_50 = list(query_up_to_50_euros)
+
+    return render(request, 'mi_app/template.html', {'listo_of_products_up_to_50': listo_of_products_up_to_50})
+
+
