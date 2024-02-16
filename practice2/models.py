f30c9019312bfc8ea966545525860aabc566d9b8
Amalia Torre <amaliatorrevillahoz@gmail.com>
2024-02-16 18:42:08 +0100

    2 exercice model

--- practice2/models.py
+++ practice2/models.py
@@ -1,2 +1,9 @@
-# Modelo: Crea un modelo llamado Producto con campos como nombre, precio y cantidad en stock.from django.db import models
+from django.db import models
+
+# Modelo: Crea un modelo llamado Producto con campos como nombre, precio y cantidad en stock.
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField()
 
