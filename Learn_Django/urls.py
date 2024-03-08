a76e8e64db7680446b82be5ff3a7718c9155b274
Amalia Torre <amaliatorrevillahoz@gmail.com>
2024-03-08 10:48:36 +0100

    wip

--- Learn_Django/urls.py
+++ Learn_Django/urls.py
@@ -1,6 +1,5 @@
 """Configura las URLs para ambas vistas (API y con templates)."""
 
-from django.contrib import admin
 from django.urls import path
 
 app_name = "books"
--- books/models.py
+++ books/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 
-""" Libro: Atributos: Título, Autor, ISBN, Descripción, Disponibilidad (booleano). Métodos: prestar(), devolver().
+""" Libro: Atributos: Título, Autor, ISBN, Descripción, Disponibilidad (booleano). 
+Métodos: prestar(), devolver().
 
-Usuario: Atributos: Nombre, Apellido, Email. Relación con Libro: Muchos a muchos 
+Usuario: Atributos: Nombre, Apellido, Email. 
+Relación con Libro: Muchos a muchos 
 (un usuario puede tener varios libros prestados, y un libro puede estar prestado a varios usuarios). """
 
 
+class Book(models.Model):
+    title = models.CharField(max_length=100, blank=False, null=False)
+    author = models.CharField(max_length=100, blank=True, null=True)
+    isbn = models.CharField(max_length=17, blank=False, null=False)
+    description = models.CharField(max_length=400, blank=True, null=True)
+    availability = models.BooleanField(default=True)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    surname = models.CharField(max_length=50, blank=False, null=False)
+    email = models.EmailField(blank=False, null=False)
+    book = models.ManyToManyField(Book, on_delete=models.CASCADE, )
